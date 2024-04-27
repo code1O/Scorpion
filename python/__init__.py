b6447b24f16b6e31c4ed0d1bfb0bd24154819aef
CodeSS0 <58243189+code1O@users.noreply.github.com>
2024-04-27 13:32:38 -0600

    code & refactor __init__.py
    optimized class accessibility functions & fixed bad calculus at `demos` class

--- python/__init__.py
+++ python/__init__.py
@@ -52,23 +52,23 @@ from functools import lru_cache
 
 class demos:
   
+  def __init__(self)-> None:...
+  
   @property
   @lru_cache()
-  def euler_approx():
-    x, y, z = 6, 12, 31.85
+  def euler_approx(self):
     f = lambda x,y,z: 2*(x**2)+(3*(x*y)+z)**2
-    custom_df = (
-    f(x+DeltaX, y+DeltaX, z+DeltaX)-f(x,y,z)
+    df = (
+    (f(6+DeltaX, 12+DeltaX, 30.85+DeltaX)-f(6,12,30.85))
     /DeltaX
-    )
-    def compute(derivative):
-      dx = derivative/1e4
+    )/1e4
+    def compute(dx):
       x,n,s = 0,1,0
       for r in range(x, 4):
         n += 1
-        (s:= dx+(r/10**n+1)-(r/10**((n+1) or 2)))
-      result = s+(10/10**5)-(9/10**6)-(2/10**8)
-      return result+(5.4e3*10**(-12))
+        s = dx+(r/10**n)+((r+1)/10**(n or 2))+(10/10**5)
+      result = s-(9/10**6)-(2/10**8)
+      return result+(5.4e3/10**12)
     
     keywords = dict(guest_name="euler_approx", host_name="euler's num")
-    return preciss(guest=compute(custom_df),host= e)
+    return preciss(guest=compute(df),host=e, keywords=keywords)
